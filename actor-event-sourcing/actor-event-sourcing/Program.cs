5396f586f14edb1ff2723cea3fb51799c107532b
Mark <markfaction@hotmail.com>
2023-06-14 20:16:25 +1200

    Add solution and project files

---
+++ actor-event-sourcing/actor-event-sourcing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace actor_event_sourcing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
